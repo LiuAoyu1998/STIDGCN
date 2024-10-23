51908b767edbe35a3ed22c643b0598626a941b48
AoyuLiu <102887543+LiuAoyu1998@users.noreply.github.com>
2024-10-23 21:18:42 +0800

    ⚡️

---
+++ model_grid.py
@@ -0,0 +1,318 @@
+import torch
+import torch.nn as nn
+from torch.nn import Conv2d, Parameter
+import torch.nn.functional as F
+import math
+
+class GLU(nn.Module):
+    def __init__(self, features, dropout=0.1):
+        super(GLU, self).__init__()
+        self.conv1 = nn.Conv2d(features, features, (1, 1))
+        self.conv2 = nn.Conv2d(features, features, (1, 1))
+        self.conv3 = nn.Conv2d(features, features, (1, 1))
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x2 = self.conv2(x)
+        out = x1 * torch.sigmoid(x2)
+        out = self.dropout(out)
+        out = self.conv3(out)
+        return out
+
+
+class TemporalEmbedding(nn.Module):
+    def __init__(self, time, features):
+        super(TemporalEmbedding, self).__init__()
+
+        self.time = time
+        self.time_day = nn.Parameter(torch.empty(time, features))
+        nn.init.xavier_uniform_(self.time_day)
+
+        self.time_week = nn.Parameter(torch.empty(7, features))
+        nn.init.xavier_uniform_(self.time_week)
+
+    def forward(self, x):
+        day_emb = x[..., 1]
+        time_day = self.time_day[(day_emb[:, :, :] * self.time).type(torch.LongTensor)]
+        time_day = time_day.transpose(1, 2).contiguous()
+
+        week_emb = x[..., 2]
+        time_week = self.time_week[(week_emb[:, :, :]).type(torch.LongTensor)]
+        time_week = time_week.transpose(1, 2).contiguous()
+
+        tem_emb = time_day + time_week
+
+        tem_emb = tem_emb.permute(0,3,1,2)
+
+        return tem_emb
+
+
+    
+class Diffusion_GCN(nn.Module):
+    def __init__(self, channels=128, diffusion_step=1, dropout=0.1):
+        super().__init__()
+        self.diffusion_step = diffusion_step
+        self.conv = nn.Conv2d(diffusion_step * channels, channels, (1, 1))
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, adj):
+        out = []
+        for i in range(0, self.diffusion_step):
+            if adj.dim() == 3:
+                x = torch.einsum("bcnt,bnm->bcmt", x, adj).contiguous()
+                out.append(x)
+            elif adj.dim() == 2:
+                x = torch.einsum("bcnt,nm->bcmt", x, adj).contiguous()
+                out.append(x)
+        x = torch.cat(out, dim=1)
+        x = self.conv(x)
+        output = self.dropout(x)
+        return output
+
+    
+class Graph_Generator(nn.Module):
+    def __init__(self, channels=128, num_nodes=170, diffusion_step=1, dropout=0.1):
+        super().__init__()
+        self.memory = nn.Parameter(torch.randn(channels, num_nodes))
+        nn.init.xavier_uniform_(self.memory)
+        self.fc = nn.Linear(2,1)
+        
+    def forward(self, x):
+        adj_dyn_1 = torch.softmax(
+            F.relu(
+                torch.einsum("bcnt, cm->bnm", x, self.memory).contiguous()
+                / math.sqrt(x.shape[1])
+            ),
+            -1,
+        )
+        adj_dyn_2 = torch.softmax(
+            F.relu(
+                torch.einsum("bcn, bcm->bnm", x.sum(-1), x.sum(-1)).contiguous()
+                / math.sqrt(x.shape[1])
+            ),
+            -1,
+        )
+        # adj_dyn = (adj_dyn_1 + adj_dyn_2 + adj)/2
+        adj_f = torch.cat([(adj_dyn_1).unsqueeze(-1)] + [(adj_dyn_2).unsqueeze(-1)], dim=-1)
+        adj_f = torch.softmax(self.fc(adj_f).squeeze(), -1)
+
+        topk_values, topk_indices = torch.topk(adj_f, k=int(adj_f.shape[1]*0.8), dim=-1)
+        mask = torch.zeros_like(adj_f)
+        mask.scatter_(-1, topk_indices, 1)
+        adj_f = adj_f * mask
+        
+        return adj_f
+
+
+class DGCN(nn.Module):
+    def __init__(self, channels=128, num_nodes=170, diffusion_step=1, dropout=0.1, emb=None):
+        super().__init__()
+        self.conv = nn.Conv2d(channels,channels,(1,1))
+        self.generator = Graph_Generator(channels, num_nodes, diffusion_step, dropout)
+        self.gcn = Diffusion_GCN(channels, diffusion_step, dropout)
+        self.emb = emb
+
+    def forward(self, x):
+        skip = x
+        x = self.conv(x)
+        adj_dyn = self.generator(x)
+        x = self.gcn(x, adj_dyn) 
+        x = x*self.emb + skip
+        return x
+
+
+class Splitting(nn.Module):
+    def __init__(self):
+        super(Splitting, self).__init__()
+
+    def even(self, x):
+        return x[:, :, :, ::2]
+
+    def odd(self, x):
+        return x[:, :, :, 1::2]
+
+    def forward(self, x):
+        return (self.even(x), self.odd(x))
+
+
+class IDGCN(nn.Module):
+    def __init__(
+        self,
+        device,
+        channels=64,
+        diffusion_step=1,
+        splitting=True,
+        num_nodes=170,
+        dropout=0.2, emb = None
+    ):
+        super(IDGCN, self).__init__()
+
+        device = device
+        self.dropout = dropout
+        self.num_nodes = num_nodes
+        self.splitting = splitting
+        self.split = Splitting()
+
+        Conv1 = []
+        Conv2 = []
+        Conv3 = []
+        Conv4 = []
+        pad_l = 3
+        pad_r = 3
+
+        k1 = 5
+        k2 = 3
+        Conv1 += [
+            nn.ReplicationPad2d((pad_l, pad_r, 0, 0)),
+            nn.Conv2d(channels, channels, kernel_size=(1, k1)),
+            nn.LeakyReLU(negative_slope=0.01, inplace=True),
+            nn.Dropout(self.dropout),
+            nn.Conv2d(channels, channels, kernel_size=(1, k2)),
+            nn.Tanh(),
+        ]
+        Conv2 += [
+            nn.ReplicationPad2d((pad_l, pad_r, 0, 0)),
+            nn.Conv2d(channels, channels, kernel_size=(1, k1)),
+            nn.LeakyReLU(negative_slope=0.01, inplace=True),
+            nn.Dropout(self.dropout),
+            nn.Conv2d(channels, channels, kernel_size=(1, k2)),
+            nn.Tanh(),
+        ]
+        Conv4 += [
+            nn.ReplicationPad2d((pad_l, pad_r, 0, 0)),
+            nn.Conv2d(channels, channels, kernel_size=(1, k1)),
+            nn.LeakyReLU(negative_slope=0.01, inplace=True),
+            nn.Dropout(self.dropout),
+            nn.Conv2d(channels, channels, kernel_size=(1, k2)),
+            nn.Tanh(),
+        ]
+        Conv3 += [
+            nn.ReplicationPad2d((pad_l, pad_r, 0, 0)),
+            nn.Conv2d(channels, channels, kernel_size=(1, k1)),
+            nn.LeakyReLU(negative_slope=0.01, inplace=True),
+            nn.Dropout(self.dropout),
+            nn.Conv2d(channels, channels, kernel_size=(1, k2)),
+            nn.Tanh(),
+        ]
+
+        self.conv1 = nn.Sequential(*Conv1)
+        self.conv2 = nn.Sequential(*Conv2)
+        self.conv3 = nn.Sequential(*Conv3)
+        self.conv4 = nn.Sequential(*Conv4)
+
+        self.dgcn = DGCN(channels, num_nodes, diffusion_step, dropout, emb)
+
+    def forward(self, x):
+        if self.splitting:
+            (x_even, x_odd) = self.split(x)
+        else:
+            (x_even, x_odd) = x
+
+        x1 = self.conv1(x_even)
+        x1 = self.dgcn(x1)
+        d = x_odd.mul(torch.tanh(x1))
+
+        x2 = self.conv2(x_odd)
+        x2 = self.dgcn(x2)
+        c = x_even.mul(torch.tanh(x2))
+
+        x3 = self.conv3(c)
+        x3 = self.dgcn(x3)
+        x_odd_update = d + x3
+
+        x4 = self.conv4(d)
+        x4 = self.dgcn(x4)
+        x_even_update = c + x4
+
+        return (x_even_update, x_odd_update)
+
+
+class IDGCN_Tree(nn.Module):
+    def __init__(
+        self, device, channels=64, diffusion_step=1, num_nodes=170, dropout=0.1
+    ):
+        super().__init__()
+
+        self.memory1 = nn.Parameter(torch.randn(channels, num_nodes, 3))
+
+        self.IDGCN = IDGCN(
+            device=device,
+            splitting=True,
+            channels=channels,
+            diffusion_step=diffusion_step,
+            num_nodes=num_nodes,
+            dropout=dropout,emb=self.memory1
+        )
+
+    def concat(self, even, odd):
+        even = even.permute(3, 1, 2, 0)
+        odd = odd.permute(3, 1, 2, 0)
+        len = even.shape[0]
+        _ = []
+        for i in range(len):
+            _.append(even[i].unsqueeze(0))
+            _.append(odd[i].unsqueeze(0))
+        return torch.cat(_, 0).permute(3, 1, 2, 0)
+
+    def forward(self, x):
+        x_even_update1, x_odd_update1 = self.IDGCN(x)
+        concat = self.concat(x_even_update1, x_odd_update1)
+        output = concat + x
+        return output
+
+
+class STAT(nn.Module):
+    def __init__(self, device, num_nodes, channels, dropout=0.1, pre_adj=None):
+        super().__init__()
+
+        # attributes
+        self.device = device
+        self.num_nodes = num_nodes
+        self.node_dim = channels
+        self.input_len = 6
+        self.input_dim = 3
+        self.output_len = 1
+        self.num_layer = 1
+        self.head = 1
+        self.num_levels = 2
+
+        self.start_conv = nn.Conv2d(
+            in_channels=self.input_dim, out_channels=channels, kernel_size=(1, 1)
+        )
+
+        if num_nodes ==1024:
+            self.time = 24
+        else:
+            self.time = 48
+
+        self.Temb = TemporalEmbedding(self.time, channels)
+
+        self.tree = IDGCN_Tree(
+            device=device,
+            channels=channels*2,
+            diffusion_step=1,
+            num_nodes=self.num_nodes,
+            dropout=dropout,
+        )
+
+        self.glu = GLU(channels*2)
+        self.regression_layer = nn.Conv2d(
+            channels*2, self.output_len, kernel_size=(1, 6)
+        )
+
+    def param_num(self):
+        return sum([param.nelement() for param in self.parameters()])
+
+    def forward(self, input):
+        x = input
+        # Encoder
+        # Data Embedding
+        time_emb = self.Temb(input.permute(0, 3, 2, 1))
+        x = torch.cat([self.start_conv(x)] + [time_emb], dim=1)
+        # IDGCN_Tree
+        x = self.tree(x)
+        # Decoder
+        gcn = self.glu(x) + x
+        prediction = self.regression_layer(F.relu(gcn))
+        return prediction
---
+++ train_grid.py
@@ -0,0 +1,331 @@
+import torch
+import numpy as np
+import pandas as pd
+import argparse
+import time
+import util
+import os
+from util import *
+import random
+from model_grid import STAT
+from ranger21 import Ranger
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--device', type=str, default='cuda:1', help='')
+parser.add_argument('--data', type=str,
+                    default='NYCTaxi_i', help='data path')
+parser.add_argument('--adjdata', type=str,
+                    default='data/PEMS08/adj_PEMS08.pkl', help='adj data path')
+parser.add_argument('--adjtype', type=str,
+                    default='doubletransition', help='adj type')
+parser.add_argument('--num_nodes', type=int,
+                    default=75, help='number of nodes')
+parser.add_argument('--channels', type=int,
+                    default=64, help='number of nodes')
+parser.add_argument('--batch_size', type=int, default=16, help='batch size')
+parser.add_argument('--learning_rate', type=float,
+                    default=0.001, help='learning rate')
+parser.add_argument('--dropout', type=float, default=0.3, help='dropout rate')
+parser.add_argument('--weight_decay', type=float,
+                    default=0.0001, help='weight decay rate')
+parser.add_argument('--epochs', type=int, default=500, help='')
+parser.add_argument('--print_every', type=int, default=200, help='')
+parser.add_argument('--save', type=str,
+                    default='./logs/'+str(time.strftime('%Y-%m-%d-%H:%M:%S'))+"-", help='save path')
+parser.add_argument('--expid', type=int, default=1, help='experiment id')
+parser.add_argument('--es_patience', type=int, default=100,
+                    help='quit if no improvement after this many iterations')
+args = parser.parse_args()
+
+def seed_it(seed):
+    random.seed(seed) #可以注释掉
+    os.environ["PYTHONSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed) #这个懂吧
+    torch.backends.cudnn.deterministic = True #确定性固定
+    torch.backends.cudnn.enabled = True  #增加运行效率，默认就是True
+    torch.manual_seed(seed)
+
+class trainer():
+    def __init__(self, scaler, num_nodes, channels, dropout, lrate, wdecay, device, pre_adj,pre_adj_t):
+        self.model = STAT(device, num_nodes, channels,
+                             dropout, pre_adj=pre_adj)
+        self.model.to(device)
+        self.optimizer = Ranger(self.model.parameters(),
+                                lr=lrate, weight_decay=wdecay)
+        # self.optimizer = optim.Adam(self.model.parameters(), lr=lrate, weight_decay=wdecay)
+        self.loss = util.masked_mae
+        self.scaler = scaler
+        self.clip = 5
+        print(self.model)
+
+    def train(self, input, real_val):
+        # input torch.Size([64, 3, 75, 6])
+        # real_val torch.Size([64, 1, 75, 1])
+        self.model.train()
+        self.optimizer.zero_grad()
+        output = self.model(input)  # 64 1 75 1
+        output = output.transpose(1, 3)  # 64 1 75 1
+        predict = self.scaler.inverse_transform(output)  # 64 1 75 1
+        loss = self.loss(predict, real_val, 0.0, 10)
+        loss.backward()
+        if self.clip is not None:
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.clip)
+        self.optimizer.step()
+        mape = util.masked_mape(predict, real_val, 0.0, 10).item()
+        rmse = util.masked_rmse(predict, real_val, 0.0, 10).item()
+        return loss.item(), mape, rmse
+
+    def eval(self, input, real_val):
+        self.model.eval()
+        output = self.model(input)
+        output = output.transpose(1, 3)
+        predict = self.scaler.inverse_transform(output)
+        loss = self.loss(predict, real_val, 0.0, 10)
+        mape = util.masked_mape(predict, real_val, 0.0, 10).item()
+        rmse = util.masked_rmse(predict, real_val, 0.0, 10).item()
+        return loss.item(), mape, rmse
+
+def main():
+
+    seed_it(6666)
+
+    data = args.data
+
+    if args.data == "NYTaxi_i":
+        args.data = "data//"+args.data
+        args.num_nodes = 75
+    
+    elif args.data == "NYTaxi_o":
+        args.data = "data//"+args.data
+        args.num_nodes = 75
+
+    elif args.data == "TDrive_i":
+        args.data = "data//"+args.data
+        args.num_nodes = 1024
+    
+    elif args.data == "TDrive_o":
+        args.data = "data//"+args.data
+        args.num_nodes = 1024
+    
+    device = torch.device(args.device)
+
+    dataloader = util.load_dataset(
+        args.data, args.batch_size, args.batch_size, args.batch_size)
+    scaler = dataloader['scaler']
+
+    pre_adj = None
+    pre_adj_t = None
+
+    loss = 9999999
+    test_log = 999999
+    epochs_since_best_mae = 0
+    path = args.save + data + "/"
+
+    his_loss = []
+    val_time = []
+    train_time = []
+    result = []
+    test_result = []
+
+    print(args)
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    engine = trainer(scaler, args.num_nodes, args.channels, args.dropout,
+                     args.learning_rate, args.weight_decay, device, pre_adj, pre_adj_t)
+
+    print("start training...", flush=True)
+
+    for i in range(1, args.epochs+1):
+
+        # train
+        train_loss = []
+        train_mape = []
+        train_rmse = []
+        t1 = time.time()
+        dataloader['train_loader'].shuffle()
+        for iter, (x, y) in enumerate(dataloader['train_loader'].get_iterator()):
+            
+            trainx = torch.Tensor(x).to(device)  # 64 12 170 1
+            trainx = trainx.transpose(1, 3)
+            trainy = torch.Tensor(y).to(device)
+            trainy = trainy.transpose(1, 3)
+            # torch.Size([64, 4, 75, 6])
+            # torch.Size([64, 2, 75, 1])
+            metrics = engine.train(trainx, trainy)
+            train_loss.append(metrics[0])
+            train_mape.append(metrics[1])
+            train_rmse.append(metrics[2])
+            if iter % args.print_every == 0:
+                log = 'Iter: {:03d}, Train Loss: {:.4f}, Train RMSE: {:.4f}, Train MAPE: {:.4f}'
+                print(log.format(
+                    iter, train_loss[-1],  train_rmse[-1], train_mape[-1]), flush=True)
+        t2 = time.time()
+        log = 'Epoch: {:03d}, Training Time: {:.4f} secs'
+        print(log.format(i, (t2-t1)))
+        train_time.append(t2-t1)
+
+        # validation
+        valid_loss = []
+        valid_mape = []
+        valid_rmse = []
+
+        s1 = time.time()
+        for iter, (x, y) in enumerate(dataloader['val_loader'].get_iterator()):
+            testx = torch.Tensor(x).to(device)
+            testx = testx.transpose(1, 3)
+            testy = torch.Tensor(y).to(device)
+            testy = testy.transpose(1, 3)
+            metrics = engine.eval(testx, testy)
+            valid_loss.append(metrics[0])
+            valid_mape.append(metrics[1])
+            valid_rmse.append(metrics[2])
+        s2 = time.time()
+
+        log = 'Epoch: {:03d}, Inference Time: {:.4f} secs'
+        print(log.format(i, (s2-s1)))
+        val_time.append(s2-s1)
+
+        mtrain_loss = np.mean(train_loss)
+        mtrain_mape = np.mean(train_mape)
+        mtrain_rmse = np.mean(train_rmse)
+
+        mvalid_loss = np.mean(valid_loss)
+        mvalid_mape = np.mean(valid_mape)
+        mvalid_rmse = np.mean(valid_rmse)
+
+        his_loss.append(mvalid_loss)
+        train_m = dict(train_loss=np.mean(train_loss), train_rmse=np.mean(train_rmse),
+                       train_mape=np.mean(train_mape), valid_loss=np.mean(valid_loss),
+                       valid_rmse=np.mean(valid_rmse), valid_mape=np.mean(valid_mape))
+        train_m = pd.Series(train_m)
+        result.append(train_m)
+
+        log = 'Epoch: {:03d}, Train Loss: {:.4f}, Train MAPE: {:.4f}, Train RMSE: {:.4f}'
+        print(log.format(i, mtrain_loss, mtrain_rmse, mtrain_mape), flush=True)
+        log = 'Epoch: {:03d}, Valid Loss: {:.4f}, Valid MAPE: {:.4f}, Valid RMSE: {:.4f}'
+        print(log.format(i, mvalid_loss, mvalid_rmse, mvalid_mape), flush=True)
+
+        if mvalid_loss < loss:
+            print("###Update tasks appear###")
+            if i < 100:
+                # It is not necessary to print the results of the test set when epoch is less than 100, because the model has not yet converged.
+                loss = mvalid_loss
+                torch.save(engine.model.state_dict(),
+                           path+"best_model.pth")
+                bestid = i
+                epochs_since_best_mae = 0
+                print("Updating! Valid Loss:", mvalid_loss, end=", ")
+                print("epoch: ", i)
+
+            elif i > 100:
+                outputs = []
+                realy = torch.Tensor(dataloader['y_test']).to(device)
+                realy = realy.transpose(1, 3) # torch.Size([64, 1, 75, 1])
+
+                for iter, (x, y) in enumerate(dataloader['test_loader'].get_iterator()):
+                    testx = torch.Tensor(x).to(device)
+                    testx = testx.transpose(1, 3)
+                    with torch.no_grad():
+                        preds = engine.model(testx).transpose(1, 3)
+                    outputs.append(preds)
+
+                yhat = torch.cat(outputs, dim=0)
+
+                yhat = yhat[:realy.size(0), ...]
+
+                amae = []
+                amape = []
+                armse = []
+                test_m = []
+                pred =  scaler.inverse_transform(yhat)
+                metrics = util.metric_grid(pred, realy)
+                amae.append(metrics[0])
+                amape.append(metrics[1])
+                armse.append(metrics[2])
+
+                log = 'Test MAE: {:.4f}, Test RMSE: {:.4f}, Test MAPE: {:.4f}'
+                print(log.format(np.mean(amae), np.mean(armse), np.mean(amape)))
+
+                if np.mean(amae) < test_log:
+                    test_log = np.mean(amae)
+                    loss = mvalid_loss
+                    torch.save(engine.model.state_dict(),
+                               path+"best_model.pth")
+                    epochs_since_best_mae = 0
+                    print("Test low! Updating! Test Loss:",
+                          np.mean(amae), end=", ")
+                    print("Test low! Updating! Valid Loss:",
+                          mvalid_loss, end=", ")
+                    bestid = i
+                    print("epoch: ", i)
+                else:
+                    epochs_since_best_mae += 1
+                    print("No update")
+
+        else:
+            epochs_since_best_mae += 1
+            print("No update")
+
+        train_csv = pd.DataFrame(result)
+        train_csv.round(6).to_csv(f'{path}/train.csv')
+        if epochs_since_best_mae >= args.es_patience and i >= 300:
+            break
+
+    # Output consumption
+    print(
+        "Average Training Time: {:.4f} secs/epoch".format(np.mean(train_time)))
+    print("Average Inference Time: {:.4f} secs".format(np.mean(val_time)))
+
+    # test
+    print("Training ends")
+    print("The epoch of the best result：", bestid)
+    print("The valid loss of the best model",
+          str(round(his_loss[bestid-1], 4)))
+
+    engine.model.load_state_dict(torch.load(path+"best_model.pth"))
+    outputs = []
+    realy = torch.Tensor(dataloader['y_test']).to(device)
+    realy = realy.transpose(1, 3) # torch.Size([64, 1, 75, 1])
+
+    for iter, (x, y) in enumerate(dataloader['test_loader'].get_iterator()):
+        testx = torch.Tensor(x).to(device)
+        testx = testx.transpose(1, 3)
+        with torch.no_grad():
+            preds = engine.model(testx).transpose(1, 3)
+        outputs.append(preds)
+
+    yhat = torch.cat(outputs, dim=0)
+    yhat = yhat[:realy.size(0), ...]
+
+
+    amae = []
+    amape = []
+    armse = []
+    test_m = []
+    pred =  scaler.inverse_transform(yhat)
+    metrics = util.metric_grid(pred, realy)
+    amae.append(metrics[0])
+    amape.append(metrics[1])
+    armse.append(metrics[2])
+
+    log = 'Test MAE: {:.4f}, Test RMSE: {:.4f}, Test MAPE: {:.4f}'
+    print(log.format(np.mean(amae), np.mean(armse), np.mean(amape)))
+
+    test_m = dict(test_loss=np.mean(amae),
+                  test_rmse=np.mean(armse), test_mape=np.mean(amape))
+    test_m = pd.Series(test_m)
+    test_result.append(test_m)
+
+    test_csv = pd.DataFrame(test_result)
+    test_csv.round(6).to_csv(f'{path}/test.csv')
+
+
+if __name__ == "__main__":
+    t1 = time.time()
+    main()
+    t2 = time.time()
+    print("Total time spent: {:.4f}".format(t2-t1))
